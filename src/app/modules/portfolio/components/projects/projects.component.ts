c3b8b33a9980381c58e771b2b470df3dbeaebfea
brunofhorn <b.fernandeshorn@gmail.com>
2024-01-09 02:51:16 -0300

    chore: create dialog component

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,28 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProject } from '../../interface/projects.interface';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  private dialog = inject(MatDialog);
   public projects = signal<IProject[]>([
     {
-      src: "assets/img/projects/vfull.png",
-      alt: "Vida FullStack",
-      title: "Vida FullStack",
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida FullStack',
+      title: 'Vida FullStack',
       width: '100px',
       height: '51px',
-      description: "",
+      description:
+        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p> <p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
       links: [
         {
-          name: "Conheça o Blog",
-          href: "https://vidafullstack.com.br"
-        }
-      ]
-    }
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
   ]);
+
+  public openDialog(data: IProject) {
+    this.dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    });
+  }
 }
